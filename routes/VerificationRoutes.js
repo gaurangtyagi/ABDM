d2d92fd0488fbf39e58ff227c79bbc91a2f9e7a6
Gaurang Tyagi <85627251+gaurangtyagi@users.noreply.github.com>
2025-03-28 13:34:14 +0530

    Update VerificationRoutes.js

--- routes/VerificationRoutes.js
+++ routes/VerificationRoutes.js
@@ -6,4 +6,7 @@ const VerificationController = require("../controllers/VerificationController");
 router.post("/send-otp", VerificationController.sendOTP);
 router.post("/verify-otp", VerificationController.verifyOTP);
 
+router.post('/request-otp-for-aadhar', VerificationController.requestOTPforAadhar);
+router.post('/verify-aadhar-otp', VerificationController.verifyAadharOTP);
+
 module.exports = router;

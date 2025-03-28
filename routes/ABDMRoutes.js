dde361fd4c21f5b3a7ea3264c628cb451a085f88
Gaurang Tyagi <85627251+gaurangtyagi@users.noreply.github.com>
2025-03-28 13:33:58 +0530

    Update ABDMRoutes.js

--- routes/ABDMRoutes.js
+++ routes/ABDMRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const ABDMController = require("../controllers/ABDMController");
+const profileController = require("../controllers/profileController");
+const { upload, uploadbuffer } = require("../middleware/uploadMiddleware");
 
 router.get("/access-token", ABDMController.fetchAccessToken); 
 router.post("/send-otp", ABDMController.sendOtp);
 router.post("/verify-otp", ABDMController.verifyOtp);
 router.post("/profile", ABDMController.getProfile);
+
+router.put('/update/profile-picture', uploadbuffer.fields([
+    { name: 'photo', maxCount: 1 },
+]),profileController.updateProfilePicture); 
+
+router.put('/update/profile-mobile', profileController.updateProfileMobile);
+router.put('/update/profile-email', profileController.updateProfilePicture); 
 
 module.exports = router;
